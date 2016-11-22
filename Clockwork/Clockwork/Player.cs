396ac50e8ccf9e93d6e9c12db9f0fb03078c2a01
TheNightglow <Marcus.Streuber@gmx.de>
2016-11-22 17:47:00 +0100

    hi

---
+++ Clockwork/Clockwork/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Clockwork
+{
+    class Player
+    {
+        Vector2 location;
+        Texture2D texture;
+
+        public Player(Texture2D tex, Vector2 pos)
+        {
+            texture = tex;
+            location = pos;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, location, Color.White);
+
+        }
+    }
+}
